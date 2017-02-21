d44b32a66157c2ff6c397aaa69d5c7191e5e4078
thomas <thomaslolkschmidt@gmail.com>
2017-02-21 01:36:33 +0100

    Initial commit

---
+++ img2text/img2txt.py
@@ -0,0 +1,33 @@
+"""
+Image to text file conversion
+
+author: Thomas Lolk Schmidt
+email: thomaslolkschmidt@gmail.com
+date: 29jan2017
+"""
+
+import matplotlib.pylab as plt
+
+# Read and plot image
+img = plt.imread('cameraman.png')
+img = img[:,:,0]*255
+plt.imshow(img, cmap='gray')
+
+# Convert image to text
+img_text = ''
+for row in img:
+    for col in row:
+        if col > 255*3/4:
+            img_text += ' '
+        elif col <= 255*3/4 and col > 255*2/4:
+            img_text += '.'
+        elif col <= 255*2/4 and col > 255*1/4:
+            img_text += '!'
+        else:
+            img_text += '#'
+    
+    img_text += '\n'
+
+# Write text to file
+with open("cameraman.txt", "w") as text_file:
+    text_file.write("{}".format(img_text))
